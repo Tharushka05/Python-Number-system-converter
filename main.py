ca56a3fba3a146fbf37cfd0530f01f35edbc00b2
Tharushka05 <77673732+Tharushka05@users.noreply.github.com>
2021-01-20 08:08:30 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+num1 = int(input("enter a number:"))
+
+re = bin(num1)
+print("In Binary =",re[2:])
+
+he = hex(num1)
+print("In Hexadecimal =",he[2:])
+
+oc = oct(num1)
+print("In Octal =",oc[2:])
+
